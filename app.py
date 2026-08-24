72348c6d7a757640033eab652a5d7774bd9afef5
Kojini502 <grazie.mille502@gmail.com>
2026-08-24 12:59:20 +0900

    Update app.py

--- app.py
+++ app.py
@@ -53,7 +53,6 @@ CRT_DATABASE = {
     },
 }
 
-import json
 import streamlit as st
 import pandas as pd
 import gspread
@@ -66,8 +65,9 @@ def save_to_gsheets(log_entry):
             "https://www.googleapis.com/auth/drive"
         ]
         
-        # JSON文字列を直接辞書オブジェクトに復元
-        creds_dict = json.loads(st.secrets["gcp_service_account"])
+        # StreamlitがTOMLセクションを自動で辞書化
+        creds_dict = dict(st.secrets["gcp_service_account"])
+        
         credentials = Credentials.from_service_account_info(creds_dict, scopes=scopes)
         gc = gspread.authorize(credentials)
         
